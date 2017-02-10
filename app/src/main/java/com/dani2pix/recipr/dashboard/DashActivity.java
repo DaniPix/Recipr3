a95795d0f0eb1feaa0eb9fdf1abcf46424770ebc
dani2pix <domnica.dan@gmail.com>
2017-02-10 15:09:28 +0200

    dashboard screen

---
+++ app/src/main/java/com/dani2pix/recipr/dashboard/DashActivity.java
@@ -0,0 +1,10 @@
+package com.dani2pix.recipr.dashboard;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Domnica on 2/10/2017.
+ */
+
+public class DashActivity extends AppCompatActivity {
+}
